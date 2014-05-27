0a1375e3345db7b23963f2e62b8b04fd8297447b
albertomarini1992 <a_marini@hotmail.it>
2014-05-27 19:20:24 +0200

    Aggiornamento

--- ScriptInserimento.sql
+++ ScriptInserimento.sql
@@ -1,11 +1,23 @@
-INSERT INTO albergo VALUES ('Accademia', 'Verona', 'via Scala 12', 0451234567, 4, 105, 120, 190, null, null, null);
-INSERT INTO albergo VALUES ('Milano', 'Verona','Vicolo Tre Marchetti 11', 045908765, 3, 80, 100, null, null, null, null); 
-INSERT INTO albergo VALUES ('B&B', 'Venezia','via Legnago', 0475278765, 3, 85, 110, null, null, null, null); 
-INSERT INTO albergo VALUES ('Franco', 'Rovigo','via MArco Polo', 0454568765, 3, 85, 110, null, null, null, null); 
-INSERT INTO albergo VALUES ('Univr', 'Treviso','via Ernesto', 045241445, 4, 85, 110, null, null, null, null); 
+#insert into Biglietto (Cod_Emissione, Data_Emissione, Importo, Tipo, Data_Inizio*, Data_Fine*, CF, CF_Abb*, Nome_P, Codice_P, Nome_A, Codice_A)
+INSERT INTO Biglietto VALUES ('Accademia', 'Verona', 'via Scala 12', 0451234567, 4, 105, 120, 190, null, null, null);
 
-INSERT INTO Camera VALUES (10, 'Accademia','Verona', 'singola' , 'libera'); 
-INSERT INTO Camera VALUES (10, 'Milano','Verona', 'quadrupla' , 'libera');
-INSERT INTO Camera VALUES (10, 'B&B','Venezia', 'doppia' , 'libera');
-INSERT INTO Camera VALUES (10, 'Franco','Rovigo', 'singola' , 'occupata');
-INSERT INTO Camera VALUES (10, 'Univr','Treviso', 'tripla' , 'occupata');
+#insert into Cliente (CF, Nome, Cognome, Data_Nascita, Comune, Login, Psw)
+INSERT INTO Cliente VALUES (10, 'Accademia','Verona', 'singola' , 'libera');
+
+#insert into Versamento (Data, Ora, Importo, CF)
+INSERT INTO Versamento VALUES (10, 'Accademia','Verona', 'singola' , 'libera');
+
+#insert into Linea (Codice, Durata_Media, Nome_A, Nome_P)
+INSERT INTO Linea VALUES (10, 'Accademia','Verona', 'singola' , 'libera'); 
+
+#insert into Corsa (Senso_Marcia, Orario_Partenza, Orario_Arrivo, Codice)
+INSERT INTO Corsa VALUES (10, 'Accademia','Verona', 'singola' , 'libera');
+
+#insert into Fermata (Nome, Codice)
+INSERT INTO Fermata VALUES (10, 'Accademia','Verona', 'singola' , 'libera');
+
+#insert into B_L (Cod_Emissione, Codice_Linea)
+INSERT INTO B_L VALUES (10, 'Accademia','Verona', 'singola' , 'libera'); 
+
+#insert into C_F (Codice_Linea, Nome, Orario)
+INSERT INTO C_F VALUES (10, 'Univr','Treviso', 'tripla' , 'occupata');
