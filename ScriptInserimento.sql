62f66105dd323880244cd128ea52b3ca50cadd33
ALESSANDRO FALDA <id727fqw@delta009.scienze.univr.it>
2014-05-27 15:00:34 +0200

    Script

---
+++ ScriptInserimento.sql
@@ -0,0 +1,11 @@
+INSERT INTO albergo VALUES ('Accademia', 'Verona', 'via Scala 12', 0451234567, 4, 105, 120, 190, null, null, null);
+INSERT INTO albergo VALUES ('Milano', 'Verona','Vicolo Tre Marchetti 11', 045908765, 3, 80, 100, null, null, null, null); 
+INSERT INTO albergo VALUES ('B&B', 'Venezia','via Legnago', 0475278765, 3, 85, 110, null, null, null, null); 
+INSERT INTO albergo VALUES ('Franco', 'Rovigo','via MArco Polo', 0454568765, 3, 85, 110, null, null, null, null); 
+INSERT INTO albergo VALUES ('Univr', 'Treviso','via Ernesto', 045241445, 4, 85, 110, null, null, null, null); 
+
+INSERT INTO Camera VALUES (10, 'Accademia','Verona', 'singola' , 'libera'); 
+INSERT INTO Camera VALUES (10, 'Milano','Verona', 'quadrupla' , 'libera');
+INSERT INTO Camera VALUES (10, 'B&B','Venezia', 'doppia' , 'libera');
+INSERT INTO Camera VALUES (10, 'Franco','Rovigo', 'singola' , 'occupata');
+INSERT INTO Camera VALUES (10, 'Univr','Treviso', 'tripla' , 'occupata');
